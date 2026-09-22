16cb19a81be534abda95d0978cbbb3710fe874d8
Alexey Kurdyukov <necroalone@gmail.com>
2026-09-22 20:23:28 +0700

    Sync Eidolarch 2.2.15

--- photomind/app.py
+++ photomind/app.py
@@ -41,7 +41,7 @@ _FS_STATS_CACHE = {}
 _FS_STATS_LOCK = threading.Lock()
 _FS_STATS_TTL = 120.0
 db.init_db()
-APP_VERSION = '2.2.14'
+APP_VERSION = '2.2.15'
 app = FastAPI(title='Eidolarch', version=APP_VERSION)
 
 @app.middleware('http')
